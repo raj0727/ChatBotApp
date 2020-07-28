4236715f4bae46fce32f6e4dc00ffcc18a8b4b9d
Rajesh Mestha <rajeshmestha82@gmail.com>
2020-07-28 20:15:42 +0000

    Added a new print statement for the Homepage

--- HomePage.java
+++ HomePage.java
@@ -4,6 +4,7 @@ public class HomePage{
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
-    }
+   System.out.println (" THis is for a change "); 
+}
 }
 
